e55b18f86010d52b9c61769b9f4de20020338297
Levid <jassontejada@gmail.com>
2023-09-03 13:30:26 -0400

    Create ClientDao

---
+++ app/src/main/java/com/levid/ticketsapp/data/local/dao/ClientDao.kt
@@ -0,0 +1,14 @@
+package com.levid.ticketsapp.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.levid.ticketsapp.data.local.entities.Client
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ClientDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(client: Client)
+}
