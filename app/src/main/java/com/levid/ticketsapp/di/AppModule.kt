6f1f8318a635d81b5afca2d2e8bd91cac34914a6
Levid <jassontejada@gmail.com>
2023-09-03 15:08:49 -0400

    Adding AppModule

---
+++ app/src/main/java/com/levid/ticketsapp/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.levid.ticketsapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.levid.ticketsapp.data.local.AppDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule{
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appDbContext: Context): AppDb =
+        Room.databaseBuilder(
+            appDbContext,
+            AppDb::class.java,
+            "AppDb")
+            .fallbackToDestructiveMigration()
+            .build()
+}
