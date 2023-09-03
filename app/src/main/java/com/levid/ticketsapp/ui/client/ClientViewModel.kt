aec2ee69174df2ddb1e4c02acf4215b6547b5ed2
Levid <jassontejada@gmail.com>
2023-09-03 14:41:27 -0400

    Adding saveClient method
    I also included annotation @HiltViewModel, create an instance for AppDb and added a variable "name" by mutableStateOf

--- app/src/main/java/com/levid/ticketsapp/ui/client/ClientViewModel.kt
+++ app/src/main/java/com/levid/ticketsapp/ui/client/ClientViewModel.kt
@@ -1,4 +1,30 @@
 package com.levid.ticketsapp.ui.client
 
-class ClientViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.levid.ticketsapp.data.local.AppDb
+import com.levid.ticketsapp.data.local.entities.Client
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ClientViewModel @Inject constructor(
+    private val appDb: AppDb
+): ViewModel() {
+
+    var name by mutableStateOf("")
+
+    fun saveClient(){
+        viewModelScope.launch {
+            val client = Client(
+                name = name
+            )
+            appDb.clientDao().save(client)
+        }
+    }
+
 }
