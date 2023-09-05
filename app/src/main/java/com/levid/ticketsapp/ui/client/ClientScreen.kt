6cbd064d98c25ee276c9e8591b25da269b1cacc9
Levid <jassontejada@gmail.com>
2023-09-04 21:47:23 -0400

    Adding a list of clients to be visible
    I added a composable method that shows a list of the clients

--- app/src/main/java/com/levid/ticketsapp/ui/client/ClientScreen.kt
+++ app/src/main/java/com/levid/ticketsapp/ui/client/ClientScreen.kt
@@ -4,23 +4,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.levid.ticketsapp.data.local.entities.Client
 
 @Composable
 fun ClientScreen(
     viewModel: ClientViewModel = hiltViewModel()
 ){
+    val clients by viewModel.clients.collectAsStateWithLifecycle()
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -28,6 +35,7 @@ fun ClientScreen(
     ) {
         NameTextField(viewModel)
         SaveButton(viewModel)
+        ShowList(clients)
     }
 
 }
@@ -52,6 +60,17 @@ fun SaveButton(viewModel: ClientViewModel) {
     ) {
         Icon(imageVector = Icons.Default.CheckCircle, contentDescription = "guardar")
         Text(text = "Guardar")
+    }
+}
+@Composable
+fun ShowList(clients: List<Client>) {
+    Text(text = "Lista de Clientes", style = MaterialTheme.typography.titleMedium)
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(clients){ client->
+            Text(text = client.name)
+        }
     }
 }
 
