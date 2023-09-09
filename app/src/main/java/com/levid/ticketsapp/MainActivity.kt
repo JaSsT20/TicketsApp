b195672c9ca16811ef4305f254d124538dda159a
Levid <jassontejada@gmail.com>
2023-09-09 17:41:45 -0400

    Cleaning MainActivity

--- app/src/main/java/com/levid/ticketsapp/MainActivity.kt
+++ app/src/main/java/com/levid/ticketsapp/MainActivity.kt
@@ -6,20 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.levid.ticketsapp.data.local.AppDb
-import com.levid.ticketsapp.di.AppModule
 import com.levid.ticketsapp.ui.client.ClientScreen
-import com.levid.ticketsapp.ui.client.ClientViewModel
 import com.levid.ticketsapp.ui.theme.TicketsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    lateinit var appDb: AppDb
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
