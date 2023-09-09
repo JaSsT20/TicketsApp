2850ee622191afbef3fbea1fa0a3965ddcf3b100
Levid <jassontejada@gmail.com>
2023-09-09 17:38:21 -0400

    Adding TypeConverter
    Solving "private final java.util.Date birthDate = null;"

--- app/src/main/java/com/levid/ticketsapp/data/local/AppDb.kt
+++ app/src/main/java/com/levid/ticketsapp/data/local/AppDb.kt
@@ -2,12 +2,15 @@ package com.levid.ticketsapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.levid.ticketsapp.data.local.dao.ClientDao
 import com.levid.ticketsapp.data.local.entities.Client
+import com.levid.ticketsapp.util.typeConverter
 
+@TypeConverters(value = [typeConverter::class])
 @Database(
     entities = [Client::class],
-    version = 1
+    version = 2
 )
 abstract class AppDb: RoomDatabase() {
     abstract fun clientDao(): ClientDao
---
+++ app/src/main/java/com/levid/ticketsapp/util/typeConverter.kt
@@ -0,0 +1,16 @@
+package com.levid.ticketsapp.util
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class typeConverter {
+    @TypeConverter
+    fun toDate(date: Long): Date{
+        return date.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun fromDate(date: Date): Long?{
+        return date?.time
+    }
+}
