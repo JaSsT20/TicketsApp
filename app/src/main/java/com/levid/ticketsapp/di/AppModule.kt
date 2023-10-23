c32dd2c332d24c006b788ff373cf4e94d4dc8f22
Levid <jassontejada@gmail.com>
2023-10-23 10:07:54 -0400

    Providing moshi and ticketsapi

--- app/src/main/java/com/levid/ticketsapp/di/AppModule.kt
+++ app/src/main/java/com/levid/ticketsapp/di/AppModule.kt
@@ -3,11 +3,16 @@ package com.levid.ticketsapp.di
 import android.content.Context
 import androidx.room.Room
 import com.levid.ticketsapp.data.local.AppDb
+import com.levid.ticketsapp.data.remote.TicketsApi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -25,4 +30,21 @@ object AppModule{
 
     @Provides
     fun provideClientDao(appDb: AppDb) = appDb.clientDao()
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideTicketApi(moshi: Moshi): TicketsApi {
+        return Retrofit.Builder()
+            .baseUrl("https://ticketsapilevid.azurewebsites.net/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TicketsApi::class.java)
+    }
 }
