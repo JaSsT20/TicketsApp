1b3c19685baa012198f07a587b42069296f653e3
Levid <jassontejada@gmail.com>
2023-10-23 10:10:24 -0400

    Adding remote methods

--- app/src/main/java/com/levid/ticketsapp/data/repositories/ClientRepository.kt
+++ app/src/main/java/com/levid/ticketsapp/data/repositories/ClientRepository.kt
@@ -2,12 +2,18 @@ package com.levid.ticketsapp.data.repositories
 
 import com.levid.ticketsapp.data.local.dao.ClientDao
 import com.levid.ticketsapp.data.local.entities.Client
+import com.levid.ticketsapp.data.remote.TicketsApi
+import com.levid.ticketsapp.data.remote.dto.ClientDto
 import javax.inject.Inject
 
 class ClientRepository @Inject constructor(
-    private val clientDao: ClientDao
+    private val clientDao: ClientDao,
+    private val ticketsApi: TicketsApi
 ) {
     suspend fun saveClient(client: Client) = clientDao.save(client)
     suspend fun deleteClient(client: Client) = clientDao.delete(client)
     fun getAll() = clientDao.getAll()
+
+    suspend fun getRemoteClients() = ticketsApi.getClients()
+    suspend fun saveRemoteClient(clientDto: ClientDto) = ticketsApi.postClient(clientDto)
 }
